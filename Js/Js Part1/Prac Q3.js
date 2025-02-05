508471b9d557ac4b72871d55f6cdf8f68edf44ee
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-05 20:59:48 +0530

    Q3

---
+++ Js/Js Part1/Prac Q3.js
@@ -0,0 +1,15 @@
+// a)
+let 1age =5;
+let 2age =10;
+
+// we cannt define the variables with numbers.
+// b)
+let marks =75;
+let isPass = True;
+// we cannot define boolean value with capital T
+
+// c)
+let isPass = 'true'
+// here true is a string instead of Boolean
+
+
