bc5334a58d31c166cb57cfa2864500768fc7e482
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-05 22:14:31 +0530

    Part2 Q3

---
+++ Js/Js Part2/pracQ3.js
@@ -0,0 +1,16 @@
+// Q1
+
+const good_string = 'apple'
+
+if(good_string[0]=='a'&& good_string.length>3){
+  console.log(`${good_string} is a good String`)
+}
+
+// Q2
+let num =12 
+if((num%3 === 0)&&(num+1 === 15)|| (num-1 === 11)){
+  console.log("safe")
+
+}else{
+  console.log("not safe")
+}
