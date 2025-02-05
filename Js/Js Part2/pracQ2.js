41f2c41c346d77ec344036ebd16398df75f3fff6
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-05 22:03:10 +0530

    Part2

---
+++ Js/Js Part2/pracQ2.js
@@ -0,0 +1,14 @@
+const size = 'XL'
+
+if(size==='XL'){
+  console.log('Rs 250')
+
+}else if(size==='L'){
+  console.log("Rs 200")
+}else if(size==="M"){
+  console.log("Rs 100")
+}else if(size === "S"){
+  "Rs 50"
+}else{
+  console.log("out of stock")
+}
