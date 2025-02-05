da4c53b9c09b1b60154cb0402b0e8fc9685d9a44
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-05 19:55:46 +0530

    added Js

---
+++ Js/Js Part1/Prac Q1.js
@@ -0,0 +1,19 @@
+// Q1
+
+let age = 23
+
+const x = age+2;
+console.log(x)
+
+// Ans= 25
+
+// Q2
+
+let hindi = 80;
+let eng = 90;
+let math = 100;
+
+let avg = (hindi+eng+math)/3
+console.log(avg)
+
+// ans = 90
