fda918847bfdabe613c06af4460d70961d0b6764
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-05 22:31:56 +0530

    Part2 Q3

--- Js/Js Part2/pracQ3.js
+++ Js/Js Part2/pracQ3.js
@@ -8,7 +8,7 @@ if(good_string[0]=='a'&& good_string.length>3){
 
 // Q2
 let num =12 
-if((num%3 === 0)&&(num+1 === 15)|| (num-1 === 11)){
+if((num%3 === 0)&&((num+1 === 15)|| (num-1 === 11))){
   console.log("safe")
 
 }else{
