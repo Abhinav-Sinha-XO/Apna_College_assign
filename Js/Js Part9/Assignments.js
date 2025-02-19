f6974c341f2a9145d58c14f34a136dcb4e45fbee
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-19 17:51:38 +0530

    js9 DOM

---
+++ Js/Js Part9/Assignments.js
@@ -0,0 +1,37 @@
+// Q1
+
+let button = document.createElement("button")
+let input = document.createElement("input")
+
+button.innerText = "Click me"
+
+// Q2
+
+button.document.setAttribute("id","btn")
+input.setAttribute("placeholder", "username")
+
+document.body.appendChild(button)
+document.body.appendChild(input)
+
+
+// Q3
+
+let btn = document.querySelector("#btn")
+
+btn.style.backgroundColor = "blue";
+btn.style.color = "white"
+
+// Q4
+
+let h1 = document.createElement("h1")
+h1.innerText = "<u>DOM Practice</u>"
+document.querySelector("body").append(h1)
+
+
+// Q5
+
+let ptag  = document.createElement("p")
+
+ptag.innerText = "APNA COLLEGE <b>DELTA</b> PRACTICE"
+
+document.querySelector("body").append(p)
